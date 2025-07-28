955cbbcfacde71f7c78e12a625a580f60df66c01
NonaProject <dinodevservices@gmail.com>
2025-07-28 21:21:46 +0300

    Added Functionality to Contact

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from nonamail import configure_email, send_message
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/send", methods=['POST'])
+def send():
+    sender_email = request.form.get('email')
+    subject = request.form.get('subject')
+    message = request.form.get('message')
+
+    fixed_message = f'Sender Email: {sender_email}\n {message}'
+
+    credentials = configure_email('', '', '')
+    send_message(credentials, subject, fixed_message)
+
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
